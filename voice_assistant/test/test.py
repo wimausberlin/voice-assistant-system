b4fba4c87234941d24b82481d0152c5c7c5c86f5
Wim <poignon.wim@gmx.de>
2021-10-22 11:23:04 +0200

    update test.py

--- voice_assistant/test/test.py
+++ voice_assistant/test/test.py
@@ -1,13 +1,31 @@
-import core.dataset
+from pandas.io import json
+from  core import dataset
+
+import os
+import pandas as pd
+import sys
 import torchaudio
 import torchaudio.transforms as T
 
+
+file_path="../../sound/1/"
+file_path_sound=file_path+"2.wav"
+
+
 def test_mfcc():
-    file_path="../../sound/0/2.wav"
-    waveform, sample_rate = torchaudio.load(file_path)
+    waveform, sample_rate = torchaudio.load(file_path_sound)
     mfcc_tranf = T.MFCC(sample_rate)
     mfcc=mfcc_tranf(waveform)
-    core.dataset.plot_spectrogram(mfcc[0])
+    dataset.plot_spectrogram(mfcc[0],"MFCC")
 
-test_mfcc()
+def test_spectAugment_mfcc():
+    spectA_tranf= dataset.SpectAugment()
+    waveform, sample_rate = torchaudio.load(file_path_sound)
+    mfcc_tranf = T.MFCC(sample_rate)
+    mfcc=mfcc_tranf(waveform)
+    spectA=spectA_tranf(mfcc)
+    dataset.plot_spectrogram(spectA[0],"SpectAugment MFCC")
 
+
+#test_mfcc()
+#test_spectAugment_mfcc()
