bfcccbf447c11b4220ac330ab712f122dd6aea0d
Wim <poignon.wim@gmx.de>
2021-12-10 15:23:22 +0100

    add test

--- voice_assistant/core/train_CNN.py
+++ voice_assistant/core/train_CNN.py
@@ -1,6 +1,6 @@
 from ast import parse
+from torch.autograd.grad_mode import no_grad
 from torch.functional import Tensor
-
 from torch.nn.modules.loss import BCELoss, CrossEntropyLoss
 from dataset import WakeWordDataset
 from model import CNNNetwork
@@ -14,7 +14,7 @@ import torchaudio.transforms as T
 import torch.nn as nn
 import torch.optim as optim
 
-def binary_accuracy(preds: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+def binary_accuracy(preds: torch.Tensor, y: torch.Tensor) -> None:
     # round predictions to the closest integer
     return (preds == y).sum() / len(y)
 
@@ -31,6 +31,33 @@ def train(model:nn.Module,dataloader:Tuple[torch.tensor,torch.tensor],criterion,
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        #acc = binary_accuracy(pred, label).item()
+
+        losses.append(loss.item())
+        preds.append(pred.detach().cpu())
+        labels.append(label.detach().cpu())
+
+        # print("Epoch: {},  Iteration: {}/{},  loss:{}".format(epoch,
+        #      idx, len(train_loader), loss))
+        pbar.set_postfix(loss=loss)
+    avg_train_loss = sum(losses)/len(losses)
+    #acc = binary_accuracy(torch.cat(preds), torch.cat(labels))
+    #print('avg train loss:', avg_train_loss, "avg train acc", acc)
+    #report = classification_report(torch.cat(labels), torch.cat(preds))
+    #print(report)
+    #return acc
+
+@torch.no_grad()
+def test(model:nn.Module,dataloader:Tuple[torch.tensor,torch.tensor],criterion,optimizer:optim,device:str,epochs:int)->None:
+    losses = []
+    preds = []
+    labels = []
+    pbar = tqdm(dataloader, desc="TEST")
+    for (input, label) in pbar:
+        input, label = input.to(device), label.to(device)
+        pred = model(input)
+        loss = criterion(pred, label)
+
         #acc = binary_accuracy(pred, label).item()
 
         losses.append(loss.item())
@@ -66,9 +93,9 @@ def main(args) -> None:
         args.train_data_json, audio_transformation=mel_spectrogram, device=device)
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size)
 
-    #test_dataset = WakeWordDataset(
-    #    args.test_data_json, audio_transformation=mel_spectrogram, device=device)
-    #test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size)
+    test_dataset = WakeWordDataset(
+        args.test_data_json, audio_transformation=mel_spectrogram, device=device)
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size)
 
     model_cnn = CNNNetwork().to(device)
 
@@ -79,9 +106,11 @@ def main(args) -> None:
     # train model
         train(model_cnn, train_dataloader, criterion,
             optimizer, device, epoch)
+        test(model_cnn, train_dataloader, criterion,
+            optimizer, device, epoch)
 
     # save model
-    torch.save(model_cnn.state_dict, args.save_path)
+    torch.save(model_cnn.state_dict(), args.save_path)
 
 
 if __name__ == "__main__":
@@ -101,8 +130,8 @@ if __name__ == "__main__":
                         help='path to save the train model')
     parser.add_argument('--train_data_json', type=str, default=None, required=True,
                         help='path to train dat json file')
-    #parser.add_argument('--test_data_json', type=str, default=None, required=True,
-    #                    help='path to train dat json file')
+    parser.add_argument('--test_data_json', type=str, default=None, required=True,
+                        help='path to train dat json file')
     parser.add_argument('--no_cuda', action='store_true', default=False,
                         help='disables CUDA training')
     args = parser.parse_args()
